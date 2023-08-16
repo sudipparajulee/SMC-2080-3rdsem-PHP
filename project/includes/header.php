12b4d90f00b1784fd194918e265f217d876c8d6e
Sudip Parajuli <sudip.parajulee@gmail.com>
2023-08-16 09:25:38 +0545

    project

---
+++ project/includes/header.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>News Portal</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <nav class="navbar">
+            <ul class="menu">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="">News</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
---
+++ project/login.php
@@ -0,0 +1,10 @@
+<?php include 'includes/header.php'; ?>
+
+<form action="" class="login-form" method="POST">
+    <h1>Login</h1>
+    <input type="text" placeholder="Username">
+    <input type="password" placeholder="Password">
+    <input type="submit" value="Login">
+</form>
+
+<?php include 'includes/footer.php'; ?>
