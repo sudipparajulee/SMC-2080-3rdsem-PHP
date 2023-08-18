bcf940d736cfaf8019c3f79886312853759c8742
Sudip Parajuli <sudip.parajulee@gmail.com>
2023-08-18 09:26:22 +0545

    Register

--- project/login.php
+++ project/login.php
@@ -6,5 +6,6 @@
     <input type="password" placeholder="Password">
     <input type="submit" value="Login">
 </form>
+<center>Don't have login? <a href="register.php">Register Here</a></center>
 
 <?php include 'includes/footer.php'; ?>
---
+++ project/register.php
@@ -0,0 +1,42 @@
+<?php include 'includes/header.php'; ?>
+
+<form action="" method="POST" class="register-form">
+    <h1>Register</h1>
+    <input type="text" name="name" placeholder="Full Name">
+    <input type="email" name="email" placeholder="Email Address">
+    <input type="password" name="password" placeholder="Password">
+    <input type="password" name="cpassword" placeholder="Confirm Password">
+    <input type="submit" name="submit" value="Register"> 
+</form>
+<center>Already have login? <a href="login.php">Login Here</a></center>
+
+<?php include 'includes/footer.php'; ?>
+
+<?php
+if(isset($_POST['submit']))
+{
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['cpassword'];
+
+    if($password == $cpassword)
+    {
+        $qry = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', 'md5($password)')";
+
+        if(mysqli_query($con, $qry))
+        {
+            echo '<script type="text/javascript"> alert("User Registered"); window.href.location = "login.php"; </script>';
+        }
+        else
+        {
+            echo '<script type="text/javascript"> alert("Error!") </script>';
+        }
+    }
+    else
+    {
+        echo '<script type="text/javascript"> alert("Password and Confirm Password does not match!"); window.href.location = "register.php"; </script>';
+    }
+}
+
+?>
