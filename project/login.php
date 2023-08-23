1ba4ced54d57115a995e542ae1845e3d1c02b17d
Sudip Parajuli <sudip.parajulee@gmail.com>
2023-08-23 09:50:11 +0545

    login

--- project/login.php
+++ project/login.php
@@ -9,3 +9,23 @@
 <center>Don't have login? <a href="register.php">Register Here</a></center>
 
 <?php include 'includes/footer.php'; ?>
+
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $qry = "SELECT * FROM users WHERE email = '$email' AND password = md5('$password')";
+    include 'includes/db.php';
+    $result = mysqli_query($con, $qry);
+    if(mysqli_num_rows($result) == 1)
+    {
+        echo "Congratulation";
+        // header('location: index.php');
+    }
+    else
+    {
+        echo '<script type="text/javascript"> alert("Invalid Credentials"); window.href.location = "login.php"; </script>';
+    }
+}
+?>
